31cfeedec3cee338281c3dacf44f47436ca6fd3f
stefan <stefan.masznitz@gmx.at>
2021-03-19 22:03:42 +0100

    implemented rook getmoves

--- ChessSimulator/ChessSimulator/Pieces/Rook.cs
+++ ChessSimulator/ChessSimulator/Pieces/Rook.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace ChessSimulator.Pieces
 {
@@ -14,15 +14,41 @@ namespace ChessSimulator.Pieces
             Colour = colour;
         }
 
-        public Position[] GetMoves(IGameBoard gameBoard, Position position)
+        private IEnumerable<Position> GetPositionsUntilNotFree(IEnumerable<BoardStateInfo> boardStateInfos) 
         {
             var result = new List<Position>();
+            foreach (var info in boardStateInfos)
+            {
+                if (info.State is null)
+                {
+                    result.Add(info.Position);
+                }
+                else if (info.State != Colour)
+                {
+                    result.Add(info.Position);
+                    break;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return result;
+        }
+
+        public Position[] GetMoves(IGameBoard gameBoard, Position position)
+        {
             var northPositions = gameBoard.GetBoardStateInfoInDirection(Direction.North, position);
             var eastPositions = gameBoard.GetBoardStateInfoInDirection(Direction.East, position);
             var southPositions = gameBoard.GetBoardStateInfoInDirection(Direction.South, position);
             var westPositions = gameBoard.GetBoardStateInfoInDirection(Direction.West, position);
 
-            return result.ToArray();
+            return GetPositionsUntilNotFree(northPositions)
+                .Concat(GetPositionsUntilNotFree(eastPositions))
+                .Concat(GetPositionsUntilNotFree(southPositions))
+                .Concat(GetPositionsUntilNotFree(westPositions))
+                .ToArray();
         }
     }
 }
