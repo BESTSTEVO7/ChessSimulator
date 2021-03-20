433c51605ce81f95874f714c4f7ad6b835d30ced
stefan <stefan.masznitz@gmx.at>
2021-03-20 13:59:50 +0100

    implemented knight logic

--- ChessSimulator/ChessSimulator.Tests/Pieces/KnightTests.cs
+++ ChessSimulator/ChessSimulator.Tests/Pieces/KnightTests.cs
@@ -1,5 +1,8 @@
-﻿using Moq;
+﻿using ChessSimulator.Pieces;
+using FluentAssertions;
+using Moq;
 using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace ChessSimulator.Tests.Pieces
 {
@@ -12,6 +15,119 @@ namespace ChessSimulator.Tests.Pieces
         public void SetUp()
         {
             gameBoard = new Mock<IGameBoard>(MockBehavior.Strict);
+        }
+
+        //| | | | | |
+        //| | | | | |
+        //| | |K| | |
+        //| | | | | |
+        //| | | | | |
+        [Test]
+        public void GetMoves_NoOtherPieces_AllMovesPossible()
+        {
+            Knight rook = new Knight(Colour.White);
+            Position knightPosition = new Position(2, 2);
+
+            gameBoard.Setup(x => x.GetBoardStateInfo(It.IsAny<Position[]>()))
+                .Returns(
+                    new List<BoardStateInfo>
+                    {
+                        new BoardStateInfo { Position = new Position(3, 0) } ,
+                        new BoardStateInfo { Position = new Position(4, 1) } ,
+                        new BoardStateInfo { Position = new Position(4, 3) } ,
+                        new BoardStateInfo { Position = new Position(3, 4) } ,
+                        new BoardStateInfo { Position = new Position(1, 4) } ,
+                        new BoardStateInfo { Position = new Position(0, 3) } ,
+                        new BoardStateInfo { Position = new Position(0, 1) } ,
+                        new BoardStateInfo { Position = new Position(1, 0) } ,
+                    });
+
+            var expectedMoves = new Position[]
+            {
+                new Position(3, 0),
+                new Position(4, 1),
+                new Position(4, 3),
+                new Position(3, 4),
+                new Position(1, 4),
+                new Position(0, 3),
+                new Position(0, 1),
+                new Position(1, 0),
+            };
+
+            var result = rook.GetMoves(gameBoard.Object, knightPosition);
+            result.Should().BeEquivalentTo(expectedMoves);
+        }
+
+        //| |B| |B| |
+        //|B| | | |B|
+        //| | |K| | |
+        //|B| | | |B|
+        //| |B| |B| |
+        [Test]
+        public void GetMoves_OnlyEnemyPieces_AllMovesPossible()
+        {
+            Knight rook = new Knight(Colour.White);
+            Position knightPosition = new Position(2, 2);
+
+            gameBoard.Setup(x => x.GetBoardStateInfo(It.IsAny<Position[]>()))
+                .Returns(
+                    new List<BoardStateInfo>
+                    {
+                        new BoardStateInfo { Position = new Position(3, 0), State = Colour.Black } ,
+                        new BoardStateInfo { Position = new Position(4, 1), State = Colour.Black } ,
+                        new BoardStateInfo { Position = new Position(4, 3), State = Colour.Black } ,
+                        new BoardStateInfo { Position = new Position(3, 4), State = Colour.Black } ,
+                        new BoardStateInfo { Position = new Position(1, 4), State = Colour.Black } ,
+                        new BoardStateInfo { Position = new Position(0, 3), State = Colour.Black } ,
+                        new BoardStateInfo { Position = new Position(0, 1), State = Colour.Black } ,
+                        new BoardStateInfo { Position = new Position(1, 0), State = Colour.Black } ,
+                    });
+
+            var expectedMoves = new Position[]
+            {
+                new Position(3, 0),
+                new Position(4, 1),
+                new Position(4, 3),
+                new Position(3, 4),
+                new Position(1, 4),
+                new Position(0, 3),
+                new Position(0, 1),
+                new Position(1, 0),
+            };
+
+            var result = rook.GetMoves(gameBoard.Object, knightPosition);
+            result.Should().BeEquivalentTo(expectedMoves);
+        }
+
+        //| |W| |W| |
+        //|W| | | |W|
+        //| | |K| | |
+        //|W| | | |W|
+        //| |W| |W| |
+        [Test]
+        public void GetMoves_OnlyAlliesPieces_NoMovePossible()
+        {
+            Knight rook = new Knight(Colour.White);
+            Position knightPosition = new Position(2, 2);
+
+            gameBoard.Setup(x => x.GetBoardStateInfo(It.IsAny<Position[]>()))
+                .Returns(
+                    new List<BoardStateInfo>
+                    {
+                        new BoardStateInfo { Position = new Position(3, 0), State = Colour.White } ,
+                        new BoardStateInfo { Position = new Position(4, 1), State = Colour.White } ,
+                        new BoardStateInfo { Position = new Position(4, 3), State = Colour.White } ,
+                        new BoardStateInfo { Position = new Position(3, 4), State = Colour.White } ,
+                        new BoardStateInfo { Position = new Position(1, 4), State = Colour.White } ,
+                        new BoardStateInfo { Position = new Position(0, 3), State = Colour.White } ,
+                        new BoardStateInfo { Position = new Position(0, 1), State = Colour.White } ,
+                        new BoardStateInfo { Position = new Position(1, 0), State = Colour.White } ,
+                    });
+
+            var expectedMoves = new Position[] { };
+
+            var result = rook.GetMoves(gameBoard.Object, knightPosition);
+            result.Should().BeEquivalentTo(expectedMoves);
         }
     }
 }
--- ChessSimulator/ChessSimulator/Pieces/Knight.cs
+++ ChessSimulator/ChessSimulator/Pieces/Knight.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Linq;
 
 namespace ChessSimulator.Pieces
 {
@@ -13,9 +13,38 @@ namespace ChessSimulator.Pieces
             Colour = colour;
         }
 
+        // order of the moves clockwise
+        // |8| |1| 
+        //7| | | |2
+        // | |K| | 
+        //6| | | |3
+        // |5| |4| 
         public Position[] GetMoves(IGameBoard gameBoard, Position position)
         {
-            throw new NotImplementedException();
+            var positions = new Position[]
+            {
+                // 1
+                new Position(position.X + 1, position.Y - 2),
+                // 2
+                new Position(position.X + 2, position.Y - 1),
+                // 3
+                new Position(position.X + 2, position.Y + 1),
+                // 4
+                new Position(position.X + 1, position.Y + 2),
+                // 5
+                new Position(position.X - 1, position.Y + 2),
+                // 6
+                new Position(position.X - 2, position.Y + 1),
+                // 7
+                new Position(position.X - 2, position.Y - 1),
+                // 8
+                new Position(position.X - 1, position.Y - 2),
+            };
+
+            return gameBoard
+                .GetBoardStateInfo(positions)
+                .Where(x => x.State is null || x.State != Colour)
+                .Select(x => x.Position).ToArray();
         }
     }
 }
